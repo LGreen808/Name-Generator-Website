31c558cfff46935650082110f144c4a73afeae2f
526LGreen@frhsd.com <526LGreen@frhsd.com>
2024-12-16 08:45:21 -0500

    Continuing to put in JavaScript

--- script.js
+++ script.js
@@ -61,13 +61,15 @@ function genFullName(){
     const newFirstName = genFirstName(firstName)
     const middleName = genMiddleName(roadType, favoriteColor)
     const newLastName = genLastName(firstName)
+    const suffix = genSuffix(favoriteAnimal)
     //Capitialize Name Variables when needed
     const capitalizedPrefix = capitalize(prefix);
     const capitalizedFirstName = capitalize(newFirstName);
     const capitalizedMiddleName = capitalize(middleName);
     const capitalizedLastName = capitalize(newLastName);
     //Combine all of the Name variables in a new name
-
+    const fullName = `${capitalizedPrefix} ${capitalizedFirstName} ${capitalizedMiddleName} ${capitalizedLastName} `
+console.log(fullName)
     //Display the new name
 }
 
